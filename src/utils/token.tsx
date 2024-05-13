28ade87c0e416e74fb239dec6e2aa08a85722004
Chaemin-L <chaem12@kookmin.ac.kr>
2024-05-14 02:25:58 +0900

    chore: .env 파일 분리 및 gitignore에 추가

--- src/utils/token.tsx
+++ src/utils/token.tsx
@@ -1,13 +1,12 @@
-import axios from "axios";
-
 import { devLog } from "./dev-log";
 
+import Instance from "@/api/axios-instance";
 import { AuthResponse } from "@/api/types/auth-type";
 
 const getRefreshToken = async () => {
   try {
-    const response = await axios.post(
-      `${process.env.REACT_APP_API_BASE_URL}:${process.env.REACT_APP_API_PORT}/auth-service/api/v2/auth/reissue`,
+    const response = await Instance.post(
+      `/auth-service/api/v2/auth/reissue`,
       {
         accessToken: localStorage.getItem("accessToken"),
         refreshToken: localStorage.getItem("refreshToken"),
