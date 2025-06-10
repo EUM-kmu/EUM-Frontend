99dd612a5c2d7d775e6a4ebba2612d5ab032cc86
unknown <masulsada@gmail.com>
2025-06-10 16:43:00 +0900

    chore: lint-fix-notice-board

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,25 @@
 // Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getMessaging } from "firebase/messaging/sw";
-import { getFirestore } from "firebase/firestore"; //Firestore 추가
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: "https://k-eum2023-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const messaging = getMessaging(app);
-const db = getFirestore(app); // 여기서 생성
+const db = getFirestore(app);
 
-export { app, auth, messaging, db }; // export
+export { app, auth, messaging, db };
